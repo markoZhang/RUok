b2653db3090df9c905d759183d6c48c4c966d8a7
Marko <swd-004@mail.foxconn.com>
2018-11-22 09:57:41 +0800

    网络框架接口创建

---
+++ latte-core/src/main/java/com/example/latte/net/HttpMethod.java
@@ -0,0 +1,16 @@
+package com.example.latte.net;
+
+/**
+ * @author Marko
+ * @date 2018/11/22
+ */
+
+public enum HttpMethod {
+    GET,
+    POST,
+    POST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UPLOAD
+}
---
+++ latte-core/src/main/java/com/example/latte/net/RestClient.java
@@ -0,0 +1,9 @@
+package com.example.latte.net;
+
+/**
+ * @author Marko
+ * @date 2018/11/21
+ */
+
+public class RestClient {
+}
